fafa0e2c14bfae243d30bc259e5075e69884c379
blogrise <han@openwolf.com>
2021-01-22 13:48:30 +0000

    logo.js: updated Url

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,10 +1,1 @@
-import React from "react"
-import { Link } from "gatsby"
-
-const Logo = (props) => (
-  <div className="site-logo">
-    <Link to="/">{props.title}</Link>
-  </div>
-)
-
-export default Logo
+[object Promise]
